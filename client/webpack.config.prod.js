04fd6d28bdb4e5621ba0cd9f27cc04a4a84a143b
Mike Bild <mike.bild@googlemail.com>
2017-10-19 21:00:28 +0200

    Dropstack deployment

--- client/webpack.config.prod.js
+++ client/webpack.config.prod.js
@@ -1,30 +1,37 @@
-const  webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: ['./src/App.js'],
+  entry: ["./src/App.js"],
   output: {
-    path: __dirname + '/build',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/build",
+    publicPath: "/",
+    filename: "bundle.js"
   },
   devServer: {
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015', 'stage-0', 'react']}},
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: { presets: ["es2015", "stage-0", "react"] }
+      }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-        GRAPHQL_URL: JSON.stringify('https://graphql-subscriptions-sse-server.services.dropstack.run'),
-      },
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        GRAPHQL_URL: JSON.stringify(
+          "https://subkit-subscriptions-server.cloud.dropstack.run"
+        )
+      }
     }),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
+      template: "src/index.html"
+    })
   ]
 };
