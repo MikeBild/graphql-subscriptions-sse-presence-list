870b62ab79267a60ecea187e13c8fe7345691ed7
Mike Bild <mike.bild@googlemail.com>
2017-10-19 20:49:12 +0200

    Add app version

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import PresenceList from './PresenceList'
+import React from "react";
+import PresenceList from "./PresenceList";
+import appPackage from "../package.json";
 
 const Layout = props => (
   <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header className="mdl-layout__header">
       <div className="mdl-layout__header-row">
-        <span className="mdl-layout-title">GraphQL SSE subscriptions - A user presence list</span>
-        <div className="mdl-layout-spacer"></div>
+        <span className="mdl-layout-title">
+          GraphQL SSE subscriptions - A user presence list
+        </span>
+        <div className="mdl-layout-spacer" />
         <nav className="mdl-navigation mdl-layout--large-screen-only">
-          <a className="mdl-navigation__link" href="https://github.com/MikeBild/graphql-subscriptions-sse-presence-list" target="_blank">GitHub</a>
+          <a
+            className="mdl-navigation__link"
+            href="https://github.com/MikeBild/graphql-subscriptions-sse-presence-list"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <span className="mdl-navigation__link">{appPackage.version}</span>
         </nav>
       </div>
     </header>
     <main className="mdl-layout__main">
-      <div className="page-content" style={{padding: '20px'}}>
+      <div className="page-content" style={{ padding: "20px" }}>
         <PresenceList />
       </div>
     </main>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
